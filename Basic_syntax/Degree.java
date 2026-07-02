2f1e0ff92bcc8c93487f16ca139e99511df3784d
hasithaea <hasitha2k4@gmail.com>
2026-07-02 18:12:22 +0530

    create repo, add basic sytax

---
+++ Basic_syntax/Degree.java
@@ -0,0 +1,5 @@
+public class Degree{
+	public static void main(String[] args){
+		System.out.println("Bachelor of Computer Science");
+	}
+}
---
+++ Basic_syntax/University.java
@@ -0,0 +1,7 @@
+public class University{
+	public static void main(String[] args){
+		System.out.println("University of Ruhuna");
+		System.out.println("Matara");
+		System.out.println("Sri Lanka");
+	}
+}
