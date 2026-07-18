fccbaf6ba0f88d79a4d30b0be1e0a9cde6f700fa
hasithaea <hasitha2k4@gmail.com>
2026-07-18 15:28:57 +0530

    inheritance example 2

--- inheritance/src/MainGPA.java
+++ inheritance/src/MainGPA.java
@@ -1,3 +1,30 @@
-void main() {
-  $END$
+class Person {
+    String name;
+    int age;
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Student extends Person {
+    double gpa;
+
+    void study() {
+        System.out.println("Studying...");
+    }
+}
+
+public class MainGPA {
+    public static void main (String [] args) {
+
+        Student s1 = new Student();
+        s1.name="Kasun";
+        s1.age=21;
+        s1.gpa=3.75;
+
+        s1.displayInfo();
+        s1.study();
+    }
 }
